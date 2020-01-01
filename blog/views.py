99b5c01dea30cbe8e4c183ba1d02562856f07a15
Rick@sahil <31406190+Sahil2rick@users.noreply.github.com>
2020-01-01 21:21:04 +0530

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
